5604cea66d974b648d3270736af824a14941b4ab
Raman But-Husaim <raman_but-husaim@live.com>
2017-04-27 22:43:34 +0300

    Add create_hand

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -30,10 +30,15 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-    case status do
-       :ok -> :erlang.binary_to_term(binary)
-       :error -> "The file does not exist"
+    case File.read(filename) do
+       {:ok, binary} -> :erlang.binary_to_term(binary)
+       {:error, _} -> "The file does not exist"
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
